08b3a1b830a0be225387e5dbf013240f69a6a4b1
solena8 <solena_ananda@hotmail.com>
2025-05-14 14:34:12 +0200

    "ajout liens origami"

--- src/data/origami.js
+++ src/data/origami.js
@@ -6,38 +6,51 @@ const origamiProjets = [
     "Tessellation Constellation",
     ["Design original", "2017"],
     "constellation.png",
-    "une photographie d'origami tessellation"
+    "une photographie d'origami tessellation",
+    "https://www.instagram.com/solena.wandering.art/p/Bie8kjgB5Er/"
   ),
   new Projet(
     "Sunflower Seed Garden",
     ["Design original", "2016"],
     "sunflower.png",
-    "une photographie d'origami tessellation"
+    "une photographie d'origami tessellation",
+    "https://www.instagram.com/solena.wandering.art/p/BJlvafuBN4p/"
   ),
   new Projet(
     "Four Leaves Tessellation",
     ["Design original", "2016"],
     "4leaf.png",
-    "une photographie d'origami tessellation"
+    "une photographie d'origami tessellation",
+    "https://www.instagram.com/solena.wandering.art/p/BUkFyFPFfTN"
   ),
   new Projet(
     "Origami Modulaire",
     ["Design original", "2019"],
     "modulaire.png",
-    "une photographie d'origami modulaire"
+    "une photographie d'origami modulaire",
+    "https://www.instagram.com/solena.wandering.art/reel/BuiSN1OjsIY/"
   ),
 
   new Projet(
     "Tree Box",
     ["Design original", "publié", "2018"],
     "treebox.png",
-    "une photographie de boite origami en forme de sapin"
+    "une photographie de boite origami en forme de sapin",
+    "https://www.instagram.com/solena.wandering.art/p/C8FsxAvNLuP/"
+  ),
+  new Projet(
+    "Arabic Tile",
+    ["Design original", "publié", "2018"],
+    "tuile.png",
+    "une photographie d'origami géométrique",
+    "https://www.instagram.com/solena.wandering.art/p/Bg4VS97FdQQ/"
   ),
   new Projet(
     "Installation à la Maison des Arts de Saint-Herblain",
     ["Installation", "2015"],
     "installation.png",
-    "soléna toussaint posant devant une installation de centaines d'origami comme coulant d'un arbre"
+    "soléna toussaint posant devant une installation de centaines d'origami comme coulant d'un arbre",
+    "https://www.instagram.com/solena.wandering.art/p/3lq1SaHqUR/"
   ),
 ];
 
